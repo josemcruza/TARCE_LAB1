49ccd5c38553ef5acfa5e4c1a7b629d285d16a21
Jose <cruzacostajosemanuel@gmail.com>
2022-11-23 18:52:09 +0000

    'v2'

---
+++ routes/db.js
@@ -0,0 +1,30 @@
+//** BD
+
+const { Pool } = require('pg');
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false
+	}
+});
+
+//** BD
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page */
+
+router.get('/', async (req, res) => {
+	try {
+		const client = await pool.connect();
+		const result = await client.query('SELECT * FROM test_table');
+		const results = { 'results': (result) ? result.rows : null};
+		res.render('db', results);
+		client.release();
+	} catch (err) {
+		console.error(err);
+		res.send("Error " + err);
+	}
+})
+module.exports = router;

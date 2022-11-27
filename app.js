d76aed49eb87512471dd896287035f7c88973b23
Jose <cruzacostajosemanuel@gmail.com>
2022-11-27 23:15:13 +0000

    'v13'

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/servicios');
 
 var dbRouter = require('./routes/db');
+var mejorasRouter = require('./routes/mejoras');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/servicios', usersRouter);
 
 app.use('/db', dbRouter);
+app.use('/mejoras', mejorasRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

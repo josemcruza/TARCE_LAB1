be055d3c9eb4e23259222306751eec2d8c81edbd
Jose <cruzacostajosemanuel@gmail.com>
2022-11-27 19:53:08 +0000

    'v11'

--- routes/servicios.js
+++ routes/servicios.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('servicios', { title: 'Express' });
+  res.render('servicios', { title: 'Servicios ofrecidos' });
 });
 
 module.exports = router;
